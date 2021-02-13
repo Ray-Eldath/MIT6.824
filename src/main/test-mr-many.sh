5d29f4196fc80da701bfa00e1bac16e7741ab51f
Cel A. Skeggs <cela@mit.edu>
2021-02-13 12:14:23 -0500

    update

--- src/main/test-mr-many.sh
+++ src/main/test-mr-many.sh
@@ -10,5 +10,10 @@ fi
 runs=$1
 chmod +x test-mr.sh
 for i in $(seq 1 $runs); do
-    timeout -k 2s 900s ./test-mr.sh
+    if ! timeout -k 2s 900s ./test-mr.sh
+    then
+        echo '***' FAILED TESTS IN TRIAL $i
+        exit 1
+    fi
 done
+echo '***' PASSED ALL $i TESTING TRIALS
